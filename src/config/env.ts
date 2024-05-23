0ba769d39b1ffc7b00fe65747fe8f07c504a7bbc
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-05-23 13:33:24 -0300

    chore: add env vars to env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -10,6 +10,14 @@ const ENVIRONMENT_SCHEMA = z.object({
   JWT_SECRET_KEY: z.string(),
   NODE_ENV: z.enum(["production", "dev", "test"]).default("dev"),
   DEFAULT_LOGO_PLACEHOLDER: z.string(),
+  MAIL_HOST: z.string().default("mailpit"),
+  MAIL_PORT: z.coerce.number().default(1025),
+  MAIL_USERNAME: z.string().default("admin"),
+  MAIL_PASSWORD: z.string().default("admin"),
+  MAIL_FROM_ADDRESS: z.string().default("contact@find-a-friend.app"),
+  MAIL_FROM_NAME: z.string().default("FindAFriend"),
+  REDIS_HOST: z.string().default("redis"),
+  REDIS_PORT: z.coerce.number().default(6379),
 });
 
 const parsedEnv = ENVIRONMENT_SCHEMA.safeParse(process.env);
