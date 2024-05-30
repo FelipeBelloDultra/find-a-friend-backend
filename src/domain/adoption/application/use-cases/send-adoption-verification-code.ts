3ef23d34a3a1c9079e1c70b1f0eb68156bec9cdc
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-05-30 12:10:28 -0300

    fix: change mail provider context

--- src/domain/adoption/application/use-cases/send-adoption-verification-code.ts
+++ src/domain/adoption/application/use-cases/send-adoption-verification-code.ts
@@ -2,10 +2,10 @@ import { Injectable } from "@nestjs/common";
 
 import { Either, right } from "~/core/either";
 import { env } from "~/config/env";
-// import { MailProvider } from "~/application/providers/mail/mail-provider";
 import { UseCase } from "~/application/use-case";
 
-// import { ExpiresAt } from "../../enterprise/entities/value-object/expires-at";
+import { SendAdoptionCodeMail } from "../mail/send-adoption-code-mail";
+import { ExpiresAt } from "../../enterprise/entities/value-object/expires-at";
 
 interface SendAdoptionVerificationCodeInput {
   petName: string;
@@ -23,20 +23,18 @@ type SendAdoptionVerificationCodeOutput = Promise<Either<OnLeft, OnRight>>;
 export class SendAdoptionVerificationCode
   implements UseCase<SendAdoptionVerificationCodeInput, SendAdoptionVerificationCodeOutput>
 {
-  public constructor() {} // private readonly mailProvider: MailProvider
+  public constructor(private readonly sendAdoptionCodeMail: SendAdoptionCodeMail) {}
 
   public async execute(input: SendAdoptionVerificationCodeInput): SendAdoptionVerificationCodeOutput {
     const link = `${env.FRONTEND_URL}/auth/adoption/${input.adoptionCode}/confirmation`;
 
-    console.log(link);
-
-    // await this.mailProvider.sendAdoptionCodeMail({
-    //   petName: input.petName,
-    //   confirmationLink: link,
-    //   adopterEmail: input.adopterEmail,
-    //   adopterName: input.adopterName,
-    //   codeExpiresAt: ExpiresAt.EXPIRATION_IN_MINUTES,
-    // });
+    await this.sendAdoptionCodeMail.send({
+      petName: input.petName,
+      confirmationLink: link,
+      adopterEmail: input.adopterEmail,
+      adopterName: input.adopterName,
+      codeExpiresAt: ExpiresAt.EXPIRATION_IN_MINUTES,
+    });
 
     return right(void 0);
   }
